f43d30b89cb4dee175b25437820ef9aac67e8a29
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2026-01-08 19:22:49 +0530

    Create page route

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import About from "@/components/About";
+import Contact from "@/components/Contact";
+import Effects from "@/components/Effects";
+import Experience from "@/components/Experience";
+import Hero from "@/components/Hero";
+import Nav from "@/components/Nav";
+import Projects from "@/components/Projects";
+import Skills from "@/components/Skills";
+
+export default function HomePage() {
+  return (
+    <>
+      <Effects />
