66b9859766c54c1367a22da5bd33341a958cedd9
Gopal Jaiswal <82341879+iitian-gopu@users.noreply.github.com>
2026-05-03 16:44:30 +0530

    Update About component

--- components/About.tsx
+++ components/About.tsx
@@ -11,3 +11,16 @@ function LocalClock() {
   useEffect(() => {
     const fmt = new Intl.DateTimeFormat("en-IN", {
       hour: "2-digit",
+      minute: "2-digit",
+      hour12: false,
+      timeZone: site.timezone,
+    });
+    const tick = () => setTime(fmt.format(new Date()));
+    tick();
+    const id = setInterval(tick, 15_000);
+    return () => clearInterval(id);
+  }, []);
+
+  return <div className="time">{time}</div>;
+}
+
